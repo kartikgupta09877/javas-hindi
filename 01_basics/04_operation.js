b326b7946d0fd95d7ff3cfcc3c7e4a6ef1e4775e
kartikgupta09877 <kartikgupta876517@gmail.com>
2025-08-31 18:00:09 +0000

    done

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,20 @@
+let someNumber=3
+let negValue=-someNumber
+
+//console.log(negValue);
+//console.log(typeof someNumber)
+
+//console.log(2+2);
+//console.log(2*2);
+//console.log(2-2);
+//console.log(2/2);
+//console.log(2**2);
+//console.log(2%2);
+
+
+let str1="kartik"
+let str2="gupta"
+
+let str3=str1 + str2
+console.log(str3);
+
